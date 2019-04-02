6da9bcce4ec19da099b7866cabd8d66f552e7977
Glitched Polygons <glitchedpolygons@protonmail.com>
2019-04-02 03:02:24 +0200

    Compiled openssl for Apple darwin-x86_64; placed into  and updated FindEmbeddedOpenSSL.cmake

--- cmake/FindEmbeddedOpenSSL.cmake
+++ cmake/FindEmbeddedOpenSSL.cmake
@@ -13,8 +13,15 @@ elseif (UNIX AND NOT APPLE)
           NAMES crypto ssl
           PATHS ${EmbeddedOpenSSL_DIR}/lib
           NO_DEFAULT_PATH)
+elseif (APPLE)
+  set(EmbeddedOpenSSL_DIR ${CMAKE_SOURCE_DIR}/dependencies/openssl/darwin-x86_64)
+  find_library(
+          EmbeddedOpenSSL_LIB
+          NAMES crypto ssl
+          PATHS ${EmbeddedOpenSSL_DIR}/lib
+          NO_DEFAULT_PATH)
 else ()
-  message("FindEmbeddedOpenSSL.cmake: No supported OpenSSL library was found for this platform inside the dependencies/openssl folder. Trying to locate OpenSSL lib externally, where exact version and compatibility with this build pipeline remain a mistery. Brace brace...")
+  message("FindEmbeddedOpenSSL.cmake: No supported embedded OpenSSL library was found for this platform inside the dependencies/openssl folder; attempting to locate OpenSSL lib externally, where the exact version and compatibility with this build pipeline remain a mistery. Brace brace...")
   find_library(
           EmbeddedOpenSSL_LIB
           NAMES crypto ssl libcrypto libssl)

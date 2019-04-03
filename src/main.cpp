5b57acf9cd6a8b7be300558786031b44b1d2de68
Raphael Beck <glitchedpolygons@gmail.com>
2019-04-04 00:40:56 +0200

    Implemented --sub, --iatm --nbf and --exp

--- src/main.cpp
+++ src/main.cpp
@@ -9,7 +9,10 @@ enum optionIndex
     HELP,
     COPY,
     ISS,
+    SUB,
     EXP,
+    IAT,
+    NBF,
     CLAIM
 };
 
@@ -18,12 +21,15 @@ const option::Descriptor usage[] = {
     {HELP,    0, "h",     "help",  option::Arg::Optional, "  -h, --help  \tPrint usage and exit."},
     {COPY,    0, "c",     "copy",  option::Arg::Optional, "  -c, --copy  \tCopy generated token to clipboard automatically."},
     {ISS,     0, "i",     "iss",   option::Arg::Optional, "  -i, --iss  \tThe issuer name of the jwt."},
-    {EXP,     0, "e",     "exp",   option::Arg::Optional, "  -e, --exp  \tThe jwt's expiration date in numeric date format, meaning the amount of SECONDS SINCE 1970-01-01T00:00:00Z UTC (according to RFC7519 standard https://tools.ietf.org/html/rfc7519#section-4.1.4). You can use https://unixtimestamp.com to your advantage."},
+    {SUB,     0, "s",     "sub",   option::Arg::Optional, "  -s, --sub  \tThe jwt's sub claim (usually whom this token refers to)."},
+    {EXP,     0, "",      "exp",   option::Arg::Optional, "  --exp  \tThe jwt's expiration date in numeric date format, meaning the amount of SECONDS SINCE 1970-01-01T00:00:00Z UTC (according to RFC7519 standard https://tools.ietf.org/html/rfc7519#section-4.1.4). You can use https://unixtimestamp.com to your advantage."},
+    {IAT,     0, "",      "iat",   option::Arg::Optional, "  --iat  \tThe numeric date format of when this token was issued. If you don't pass this argument, it defaults to the current time in UTC."},
+    {NBF,     0, "",      "nbf",   option::Arg::Optional, "  --nbf  \tDatetime of when the jwt starts being valid (in numeric date format, just as in the --exp argument)."},
     {CLAIM,   0, "",      "claim", option::Arg::Optional, "  --claim    \tPut as many claims in as you need. Specify them with the syntax \"--claim=CLAIM_NAME:CLAIM_VALUE\" (without quotation marks)."},
     {UNKNOWN, 0, "",      "",      option::Arg::None,     "\nExamples:"
                                                           "\n  jwtgen --iss=glitchedpolygons --copy"
                                                           "\n  jwtgen --iss=otherIssuerName --claim=role:admin --claim=projectId:7"
-                                                          "\n  jwtgen -iglitchedtime -c --exp=1587399600"},
+                                                          "\n  jwtgen -iglitchedtime -c --exp=1587399600\n\nFully qualified arguments (double-dash) need to have the equals sign '=' between them and their values."},
 
     {0,       0, nullptr, nullptr, nullptr,               nullptr}
 };
@@ -56,10 +62,21 @@ static vector<string> split(const string& inputString, char delimiter)
 }
 
 /**
+ * Tries to convert a string (raw char*) into a time_point (parsing it as long int).
+ * @param str The unix time string (seconds since 1970-01-01T00:00:00Z) to convert.
+ * @return The converted time_point.
+ */
+inline static std::chrono::system_clock::time_point string_to_time_point(const char* str)
+{
+    const long int s = std::strtol(str, nullptr, 10);
+    return std::chrono::system_clock::from_time_t(std::abs(s));
+}
+
+/**
  * Parses the passed jwt generation parameters and outputs the final token (in its encoded form ready for usage).<p>
  * @param argc The amount of passed CLI arguments.
  * @param argv The passed program's CLI arguments.
- * @return 0 if generation was successful or the user asked for help; 1 if the program's arguments couldn't be parsed; 2 if you passed more than one issuer argument.
+ * @return 0 if generation was successful or the user asked for help; 1 if the program's arguments couldn't be parsed; 2 if the passed arguments are invalid.
  */
 int main(int argc, char** argv)
 {
@@ -104,9 +121,57 @@ int main(int argc, char** argv)
         token.set_issuer(iss->arg);
     }
 
-    for (const Option* option = options[CLAIM]; option; option = option->next())
+    const Option* sub = options[SUB];
+    if (sub != nullptr)
     {
-        const string& claim(option->arg);
+        if (sub->count() > 1)
+        {
+            cout << "\nERROR: You passed more than one subject. Only one --sub per jwt is allowed!\n";
+            return 2;
+        }
+        token.set_subject(sub->arg);
+    }
+
+    const Option* exp = options[EXP];
+    if (exp != nullptr)
+    {
+        if (exp->count() > 1)
+        {
+            cout << "\nERROR: You passed more than one expiration datetime. Only one --exp argument per jwt is allowed!\n";
+            return 2;
+        }
+        token.set_expires_at(string_to_time_point(exp->arg));
+    }
+
+    const Option* iat = options[IAT];
+    if (iat != nullptr)
+    {
+        if (iat->count() > 1)
+        {
+            cout << "\nERROR: You passed more than one issued-at datetime. Only one --iat argument per jwt is allowed!\n";
+            return 2;
+        }
+        token.set_issued_at(string_to_time_point(iat->arg));
+    }
+    else
+    {
+        token.set_issued_at(std::chrono::system_clock::now());
+    }
+
+    const Option* nbf = options[NBF];
+    if (nbf != nullptr)
+    {
+        if (nbf->count() > 1)
+        {
+            cout << "\nERROR: You passed more than one not-before (nbf) datetime. Only one --nbf argument per jwt is allowed!\n";
+            return 2;
+        }
+        token.set_not_before(string_to_time_point(nbf->arg));
+    }
+
+    for (const Option* opt = options[CLAIM]; opt; opt = opt->next())
+    {
+        const string& claim(opt->arg);
         if (claim.empty())
         {
             cout << "WARNING: One or more claims were left empty (--claim= )";
@@ -116,13 +181,14 @@ int main(int argc, char** argv)
         const vector<string>& kvp = split(claim, ':');
         if (kvp.size() != 2)
         {
-            cout << "ERROR: Invalid claim argument \"" << claim << "\" - please use the correct syntax:  --claim=CLAIM_NAME:CLAIM_VALUE   the colon : delimiter is important. No spaces!";
-            continue;
+            cout << "ERROR: Invalid claim argument \"" << claim << "\" - please use the correct syntax:  --claim=CLAIM_NAME:CLAIM_VALUE   the colon : delimiter is important. No spaces! No weird characters!";
+            return 2;
         }
+
         if (kvp[0].empty() || kvp[1].empty())
         {
-            cout << "ERROR: Invalid claim argument \"" << claim << "\" - claim name or claim value is an empty string. Please use the correct syntax:  --claim=CLAIM_NAME:CLAIM_VALUE   the colon : delimiter is important. No spaces!";
-            continue;
+            cout << "ERROR: Invalid claim argument \"" << claim << "\" - claim name or claim value is an empty string. Please use the correct syntax:  --claim=CLAIM_NAME:CLAIM_VALUE   the colon delimiter is important. No spaces! No weird characters!";
+            return 2;
         }
 
         token.set_payload_claim(kvp[0], jwt::claim(kvp[1]));

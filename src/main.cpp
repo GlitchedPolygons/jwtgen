bd7b259afcef4897a1fc63769ba9b5f17192fd20
Raphael Beck <glitchedpolygons@gmail.com>
2019-04-03 20:44:06 +0200

    Reformatted code to look much nicer + updated CLion code style template (Project.xml).

--- src/main.cpp
+++ src/main.cpp
@@ -3,23 +3,29 @@
 #include "jwt-cpp/jwt.h"
 #include "optionparser.h"
 
-enum optionIndex {
-  UNKNOWN, HELP, COPY, ISS, EXP, CLAIM
+enum optionIndex
+{
+    UNKNOWN,
+    HELP,
+    COPY,
+    ISS,
+    EXP,
+    CLAIM
 };
 
 const option::Descriptor usage[] = {
-        {UNKNOWN, 0, "",      "",      option::Arg::None,     "\nUsage:  \tjwtgen [options]\n\n" "Options:"},
-        {HELP,    0, "h",     "help",  option::Arg::Optional, "  -h, --help  \tPrint usage and exit."},
-        {COPY,    0, "c",     "copy",  option::Arg::Optional, "  -c, --copy  \tCopy generated token to clipboard automatically."},
-        {ISS,     0, "i",     "iss",   option::Arg::Optional, "  -i, --iss  \tThe issuer name of the jwt."},
-        {EXP,     0, "e",     "exp",   option::Arg::Optional, "  -e, --exp  \tThe jwt's expiration date in numeric date format, meaning the amount of SECONDS SINCE 1970-01-01T00:00:00Z UTC (according to RFC7519 standard https://tools.ietf.org/html/rfc7519#section-4.1.4). You can use https://unixtimestamp.com to your advantage."},
-        {CLAIM,   0, "",      "claim", option::Arg::Optional, "  --claim    \tPut as many claims in as you need. Specify them with the syntax \"--claim=CLAIM_NAME:CLAIM_VALUE\" (without quotation marks)."},
-        {UNKNOWN, 0, "",      "",      option::Arg::None,     "\nExamples:"
-                                                              "\n  jwtgen --iss=glitchedpolygons --copy"
-                                                              "\n  jwtgen --iss=otherIssuerName --claim=role:admin --claim=projectId:7"
-                                                              "\n  jwtgen -iglitchedtime --exp=1587399600"},
+    {UNKNOWN, 0, "",      "",      option::Arg::None,     "\nUsage:  \tjwtgen [options]\n\n" "Options:"},
+    {HELP,    0, "h",     "help",  option::Arg::Optional, "  -h, --help  \tPrint usage and exit."},
+    {COPY,    0, "c",     "copy",  option::Arg::Optional, "  -c, --copy  \tCopy generated token to clipboard automatically."},
+    {ISS,     0, "i",     "iss",   option::Arg::Optional, "  -i, --iss  \tThe issuer name of the jwt."},
+    {EXP,     0, "e",     "exp",   option::Arg::Optional, "  -e, --exp  \tThe jwt's expiration date in numeric date format, meaning the amount of SECONDS SINCE 1970-01-01T00:00:00Z UTC (according to RFC7519 standard https://tools.ietf.org/html/rfc7519#section-4.1.4). You can use https://unixtimestamp.com to your advantage."},
+    {CLAIM,   0, "",      "claim", option::Arg::Optional, "  --claim    \tPut as many claims in as you need. Specify them with the syntax \"--claim=CLAIM_NAME:CLAIM_VALUE\" (without quotation marks)."},
+    {UNKNOWN, 0, "",      "",      option::Arg::None,     "\nExamples:"
+                                                          "\n  jwtgen --iss=glitchedpolygons --copy"
+                                                          "\n  jwtgen --iss=otherIssuerName --claim=role:admin --claim=projectId:7"
+                                                          "\n  jwtgen -iglitchedtime --exp=1587399600"},
 
-        {0,       0, nullptr, nullptr, nullptr,               nullptr}
+    {0,       0, nullptr, nullptr, nullptr,               nullptr}
 };
 
 using std::cout;
@@ -33,7 +39,8 @@ static vector<string> split(const string& inputString, char delimiter)
     string token;
     vector<string> tokens;
     istringstream tokenStream(inputString);
-    while (std::getline(tokenStream, token, delimiter)) {
+    while (std::getline(tokenStream, token, delimiter))
+    {
         tokens.push_back(token);
     }
     return tokens;
@@ -48,8 +55,8 @@ static vector<string> split(const string& inputString, char delimiter)
  */
 int main(int argc, char** argv)
 {
-    argc -= (argc>0);
-    argv += (argc>0); // skip program name argv[0] if present.
+    argc -= (argc > 0);
+    argv += (argc > 0); // skip program name argv[0] if present.
 
     using option::Stats;
     using option::Option;
@@ -59,43 +66,52 @@ int main(int argc, char** argv)
     Option options[128], buffer[4096];
     Parser parse(usage, argc, argv, options, buffer);
 
-    if (parse.error()) {
+    if (parse.error())
+    {
         cout << "\nERROR: Failed to parse jwtgen command line arguments.";
         return 1;
     }
 
-    if (options[HELP] || argc==0) {
+    if (options[HELP] || argc == 0)
+    {
         option::printUsage(cout, usage);
         return 0;
     }
 
-    for (Option* opt = options[UNKNOWN]; opt; opt = opt->next()) {
+    for (Option* opt = options[UNKNOWN]; opt; opt = opt->next())
+    {
         cout << "Unknown option: " << opt->name << "\n";
     }
 
     jwt::builder token = jwt::create();
 
     Option* iss = options[ISS];
-    if (iss!=nullptr) {
-        if (iss->count()>1) {
+    if (iss != nullptr)
+    {
+        if (iss->count() > 1)
+        {
             cout << "\nERROR: You passed more than one issuer. Only one issuer per jwt is allowed!\n";
             return 2;
         }
         token.set_issuer(iss->arg);
     }
 
-    for (Option* opt = options[CLAIM]; opt; opt = opt->next()) {
+    for (Option* opt = options[CLAIM]; opt; opt = opt->next())
+    {
         const string& s(opt->arg);
-        if (s.empty()) {
+        if (s.empty())
+        {
             continue;
         }
         const vector<string>& spl = split(s, ':');
-        if (spl.size()!=2) {
+        if (spl.size() != 2)
+        {
             cout << "ERROR: invalid claim argument \"" << s
                  << "\" - please use the correct syntax:  --claim=CLAIM_NAME:CLAIM_VALUE   the colon : delimiter is important. No spaces!";
             continue;
         }
-        if (spl[0].empty() || spl[1].empty()) {
+        if (spl[0].empty() || spl[1].empty())
+        {
             cout << "ERROR: invalid claim argument \"" << s
                  << "\" - claim name or claim value is an empty string. Please use the correct syntax:  --claim=CLAIM_NAME:CLAIM_VALUE   the colon : delimiter is important. No spaces!";
             continue;
@@ -103,10 +119,11 @@ int main(int argc, char** argv)
         token.set_payload_claim(spl[0], jwt::claim(spl[1]));
     }
 
-    const string& output = token.sign(jwt::algorithm::hs256 {"secretsecretsecret"});
+    const string& output = token.sign(jwt::algorithm::hs256 {"secret"});
     cout << endl << output;
 
-    if (options[COPY]) {
+    if (options[COPY])
+    {
         // TODO: copy the jwt to the clipboard here
     }
 

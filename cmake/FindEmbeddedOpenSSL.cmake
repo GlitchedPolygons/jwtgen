fc5118ef1483f7b6c204c294645ac9d2217f544e
Glitched Polygons <glitchedpolygons@protonmail.com>
2019-04-06 13:47:19 +0200

    Simplified FindEmbeddedOpenSSL.cmake script

--- cmake/FindEmbeddedOpenSSL.cmake
+++ cmake/FindEmbeddedOpenSSL.cmake
@@ -1,31 +1,11 @@
 if (CYGWIN)
   set(EmbeddedOpenSSL_DIR ${CMAKE_SOURCE_DIR}/dependencies/openssl/cygwin-x86_64)
-  find_library(
-          EmbeddedOpenSSL_LIB
-          NAMES crypto ssl
-          PATHS ${EmbeddedOpenSSL_DIR}/lib
-          NO_DEFAULT_PATH)
 elseif (WIN32)
   set(EmbeddedOpenSSL_DIR ${CMAKE_SOURCE_DIR}/dependencies/openssl/windows-x86_64)
-  find_library(
-          EmbeddedOpenSSL_LIB
-          NAMES libcrypto libssl
-          PATHS ${EmbeddedOpenSSL_DIR}/lib
-          NO_DEFAULT_PATH)
 elseif (UNIX AND NOT APPLE)
   set(EmbeddedOpenSSL_DIR ${CMAKE_SOURCE_DIR}/dependencies/openssl/linux-x86_64)
-  find_library(
-          EmbeddedOpenSSL_LIB
-          NAMES crypto ssl
-          PATHS ${EmbeddedOpenSSL_DIR}/lib
-          NO_DEFAULT_PATH)
 elseif (APPLE)
   set(EmbeddedOpenSSL_DIR ${CMAKE_SOURCE_DIR}/dependencies/openssl/darwin-x86_64)
-  find_library(
-          EmbeddedOpenSSL_LIB
-          NAMES crypto ssl
-          PATHS ${EmbeddedOpenSSL_DIR}/lib
-          NO_DEFAULT_PATH)
 else ()
   message("FindEmbeddedOpenSSL.cmake: No supported embedded OpenSSL library was found for this platform inside the dependencies/openssl folder; attempting to locate OpenSSL lib externally, where the exact version and compatibility with this build pipeline remain a mistery. Brace brace...")
   find_library(
@@ -39,6 +19,18 @@ else ()
   endif ()
 endif ()
 
-message(FindEmbeddedOpenSSL.cmake:\ Found\ OpenSSL\ Path:\ ${EmbeddedOpenSSL_LIB})
+if (NOT EmbeddedOpenSSL_LIB)
+  find_library(
+          EmbeddedOpenSSL_LIB
+          NAMES crypto ssl libcrypto libssl
+          PATHS ${EmbeddedOpenSSL_DIR}/lib
+          NO_DEFAULT_PATH)
+endif ()
+
+if (NOT EmbeddedOpenSSL_LIB)
+  message("ERROR: OpenSSL dependency not found. Please make sure to install and setup dependencies first!")
+else ()
+  message(FindEmbeddedOpenSSL.cmake:\ Found\ OpenSSL\ Path:\ ${EmbeddedOpenSSL_LIB})
+endif ()
 
 set(EmbeddedOpenSSL_INCLUDE_DIR ${EmbeddedOpenSSL_DIR}/include)
